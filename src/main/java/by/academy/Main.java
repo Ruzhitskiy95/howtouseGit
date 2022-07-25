1823869543a266fb2cb9e315f8acc0fc4832f9ea
Ruzhitskiy95 <th1463975@yandex.ru>
2022-07-25 14:49:12 +0300

    Revert "Hi to Hello secondBranch"
    This reverts commit fb261e26

--- src/main/java/by/academy/Main.java
+++ src/main/java/by/academy/Main.java
@@ -3,6 +3,5 @@ package by.academy;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello secondBranch!");
-        System.out.println("Hello testBranch!");
     }
 }
